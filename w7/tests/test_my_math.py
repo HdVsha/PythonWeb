c96456e6b7cf1d92d3eff33e233a17e301a2ace3
HdVsha <ageev_ramil@mail.ru>
2020-10-26 19:30:23 +0300

    committing progress

--- w7/tests/test_my_math.py
+++ w7/tests/test_my_math.py
@@ -1,2 +1,8 @@
+import pytest
+from my_mathematics.math import MyMath
+import math
 
 
+@pytest.mark.parametrize("value", [0, 1, 90, -1, -90, 1000, 40, 50])
+def test_math_lib(value):
+    assert MyMath.sin(value) == math.sin(value)
