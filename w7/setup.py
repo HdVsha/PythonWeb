1f2a518fb0d6472b0f427e844db3053bf6bfa98f
HdVsha <ageev_ramil@mail.ru>
2020-10-26 19:21:06 +0300

    commiting my files

---
+++ w7/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+
+
+setuptools.setup(
+    name="HdVsha", # Replace with your own username
+    version="0.0.1",
+    author="Ramil",
+    author_email="ageev_ramil@mail.ru",
+    description="A small example package",
+    long_description_content_type="text/markdown",
+    url="https://github.com/HdVsha",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
