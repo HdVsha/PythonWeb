08c3704a97ab84887ec7a5164396e1eb73f3cf37
HdVsha <ageev_ramil@mail.ru>
2020-11-02 14:45:08 +0300

    finished creating func with compress

--- w8/tasks/CompressTask.py
+++ w8/tasks/CompressTask.py
@@ -1,26 +1,23 @@
-from itertools import combinations_with_replacement
+from itertools import groupby
 
 
-def get_combinations_with_r(s, n):
-    return sorted([''.join(sorted(x)) for x in combinations_with_replacement(s, n)])
+def compress_string(s):
+    groups = []
+    uniquekeys = []
+    for k, g in groupby(s):
+        groups.append(len(list(g)))
+        uniquekeys.append(int(k))
+    return list(zip(groups, uniquekeys))
 
 
-def test_get_combinations_with_r():
-    assert get_combinations_with_r('cat', 2) == ['aa', 'ac', 'at', 'cc', 'ct', 'tt']
-    assert get_combinations_with_r('dog', 3) == ['ddd', 'ddg', 'ddo', 'dgg', 'dgo',
-                                                 'doo', 'ggg', 'ggo', 'goo', 'ooo']
-    assert get_combinations_with_r('some', 5) == ['eeeee', 'eeeem', 'eeeeo', 'eeees', 'eeemm',
-                                                  'eeemo', 'eeems', 'eeeoo', 'eeeos', 'eeess',
-                                                  'eemmm', 'eemmo', 'eemms', 'eemoo', 'eemos',
-                                                  'eemss', 'eeooo', 'eeoos', 'eeoss', 'eesss',
-                                                  'emmmm', 'emmmo', 'emmms', 'emmoo', 'emmos',
-                                                  'emmss', 'emooo', 'emoos', 'emoss', 'emsss',
-                                                  'eoooo', 'eooos', 'eooss', 'eosss', 'essss',
-                                                  'mmmmm', 'mmmmo', 'mmmms', 'mmmoo', 'mmmos',
-                                                  'mmmss', 'mmooo', 'mmoos', 'mmoss', 'mmsss',
-                                                  'moooo', 'mooos', 'mooss', 'mosss', 'mssss',
-                                                  'ooooo', 'oooos', 'oooss', 'oosss', 'ossss', 'sssss']
+def test_compress_string():
+    assert compress_string('1222311') == [(1, 1), (3, 2), (1, 3), (2, 1)]
+    assert compress_string('12223111') == [(1, 1), (3, 2), (1, 3), (3, 1)]
+    assert compress_string('144423455553333532341') == [(1, 1), (3, 4), (1, 2), (1, 3),
+                                                 (1, 4), (4, 5), (4, 3), (1, 5),
+                                                 (1, 3), (1, 2), (1, 3), (1, 4),
+                                                 (1, 1)]
 
 
 if __name__ == "__main__":
-    test_get_combinations_with_r()
+    test_compress_string()
