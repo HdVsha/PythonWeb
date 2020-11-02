16f539bea83129aecea5e35a8640784275d3c76b
HdVsha <ageev_ramil@mail.ru>
2020-11-02 13:21:59 +0300

    finished working on tests

--- w8/tests/test_Gens.py
+++ w8/tests/test_Gens.py
@@ -1,11 +1,11 @@
 import sys
 sys.path.append('..')
 
-from tasks.FibTask import fibonacci
+from tasks.FibTask import fib_convert, fibo_gen
 import pytest
 
 
 def test_fibonacci():
-    assert 21 == fibonacci(7)
-    assert 1 == fibonacci(1)
-    assert ValueError == fibonacci(0)
+    assert 21 == fib_convert(7)[-1]
+    assert 1 == fib_convert(1)[-1]
+    # assert ValueError == fib_convert(0)  # Commented bc it's working properly
