81c98a46e7c89035afc21a2f8d1ab0424e1e7904
HdVsha <ageev_ramil@mail.ru>
2020-11-02 14:38:06 +0300

    finished creating func with combinations

---
+++ w8/tasks/CombinTask.py
@@ -0,0 +1,16 @@
+from itertools import combinations
+
+
+def get_combinations(s, n):
+    a = sorted([''.join(sorted(x)) for i in range(1, n+1) for x in combinations(s, i)])
+    a.sort(key=lambda x: len(x))
+    return a
+
+
+def test_combinations():
+    assert get_combinations('cat', 2) == ['a', 'c', 't', 'ac', 'at', 'ct']
+    assert get_combinations('dog', 3) == ['d', 'g', 'o', 'dg', 'do', 'go', 'dgo']
+
+
+if __name__ == "__main__":
+    test_combinations()
