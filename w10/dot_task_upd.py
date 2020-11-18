3dcdaf57925eb3b9e484a7369dc5a4cb3ca3b1c0
HdVsha <ageev_ramil@mail.ru>
2020-11-18 15:15:57 +0300

    Finish 3rd Code Task final

--- w10/dot_task_upd.py
+++ w10/dot_task_upd.py
@@ -7,10 +7,21 @@ if __name__ == "__main__":
     def dot(v1, v2):
         return np.dot(v1, v2)
 
-    def threaded_dot(array, thread):
+
+    def split_array(v1, v2, pieces_number):
+        """
+        >>> split_array([1, 2, 3, 4], 2)
+        >>> [1, 2], [3, 4]
+        """
+        res_v1, res_v2 = np.split(np.array(v1), pieces_number), np.split(np.array(v2), pieces_number)
+        for i in range(len(res_v1)):
+            if res_v1[i].size > 0:
+                yield (res_v1[i], res_v2[i])
+
+    def threaded_dot(mas, th):
         threads = []
-        for attempt in range(thread):
-            threads.append(threading.Thread(target=dot, args=(array[0], array[1],)))
+        for subarray1, subarray2 in split_array(mas[0], mas[1], th):
+            threads.append(threading.Thread(target=dot, args=(np.array(subarray1), np.array(subarray2),)))
             # threads.append(multiprocessing.Process(target=sum_a, args=(subarray, )))
             threads[-1].start()
 
