9dd125d05714262788d3eaf2b1e8649af9ac4809
HdVsha <62552291+HdVsha@users.noreply.github.com>
2020-12-01 01:34:10 +0300

    Task 3: Update

--- w11/aioread_task.py
+++ w11/aioread_task.py
@@ -5,19 +5,19 @@ import aiohttp
 
 async def fetch(client, service):
     async with client.get(service) as resp:
-        text = resp.text()
+        text = await resp.text()
 
         for line in text:
             if line.startswith('a '):
-                async with aiofile.async_open('found.txt', 'a') as file:
-                    file.write(f'From: {service}\n What: {line} \n')
+                async with aiofile.AIOFile('found.txt', 'w') as file:
+                    await file.write(f'From: {service}\nWhat: {line} \n')
 
 
 async def async_read(file_name):
-    async with aiofile.async_open(file_name, 'r') as f:
-        async for line in aiofile.LineReader(f):
-            async with aiohttp.ClientSession() as session:
-                asyncio.ensure_future(fetch(session, line))
+    async with aiofile.AIOFile(file_name, 'r') as f:
+        async with aiohttp.ClientSession() as session:
+            async for line in aiofile.LineReader(f):
+                await asyncio.ensure_future(fetch(session, line))
 
 
 if __name__ == "__main__":
